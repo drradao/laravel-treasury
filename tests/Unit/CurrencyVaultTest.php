02ea27962defd9bbd9bc000ac4d404a26fbdcccb
David Adão <drradao@gmail.com>
2024-02-01 00:40:31 +0000

    :white_check_mark: adds tests

--- tests/Unit/CurrencyVaultTest.php
+++ tests/Unit/CurrencyVaultTest.php
@@ -3,7 +3,7 @@
 use Drradao\LaravelTreasury\CurrencyVault;
 use Drradao\LaravelTreasury\Tests\Models\User;
 
-test('currency vault can credit', function () {
+it('can add to balance', function () {
     $user = User::factory()->create();
     $currencyVault = new CurrencyVault(
         owner: $user,
@@ -14,3 +14,51 @@ test('currency vault can credit', function () {
 
     expect($currencyVault->balance())->toBe(100);
 });
+
+it('can subtract from balance', function () {
+    $user = User::factory()->create();
+    $currencyVault = new CurrencyVault(
+        owner: $user,
+        currencyName: 'credits',
+    );
+
+    $currencyVault->credit(100);
+    $currencyVault->debit(50);
+
+    expect($currencyVault->balance())->toBe(50);
+});
+
+it('doesnot allow negative balance', function () {
+    $user = User::factory()->create();
+    $currencyVault = new CurrencyVault(
+        owner: $user,
+        currencyName: 'credits',
+    );
+
+    $currencyVault->debit(100);
+})->throws(\Drradao\LaravelTreasury\Exceptions\InsufficientFunds::class);
+
+it('doesn\'t allow negative amounts', function () {
+    $user = User::factory()->create();
+    $currencyVault = new CurrencyVault(
+        owner: $user,
+        currencyName: 'credits',
+    );
+
+    expect(fn () => $currencyVault->credit(-100))
+        ->toThrow(\Drradao\LaravelTreasury\Exceptions\NegativeAmoutPassed::class);
+    expect(fn () => $currencyVault->debit(-100))
+        ->toThrow(\Drradao\LaravelTreasury\Exceptions\NegativeAmoutPassed::class);
+});
+
+it('validates the max balance', function () {
+    config()->set('treasury.currencies.credits.max_balance', 100);
+
+    $user = User::factory()->create();
+    $currencyVault = new CurrencyVault(
+        owner: $user,
+        currencyName: 'credits',
+    );
+
+    $currencyVault->credit(101);
+})->throws(\Drradao\LaravelTreasury\Exceptions\ExceedsMaximumBalance::class);
